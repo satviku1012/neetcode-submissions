9d36a0d56a7d3cef3eb9f8c7fae47a03874f5d96
satviku1012 <125938100+satviku1012@users.noreply.github.com>
2026-09-15 18:31:46 -0500

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.data = []
+        self.minNum = float("infinity")
+        self.prefixMin = []
+
+    def push(self, val: int) -> None:
+        self.data.append(val)
+        self.minNum = min(self.minNum, val)
+        self.prefixMin.append(self.minNum)
+
+    def pop(self) -> None:
+        self.prefixMin.pop()
+        if len(self.prefixMin) > 0:
+            self.minNum = self.prefixMin[-1]
+        self.data.pop()
+
+    def top(self) -> int:
+        return self.data[-1]
+
+    def getMin(self) -> int:
+        return self.prefixMin[-1]
